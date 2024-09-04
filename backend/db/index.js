8534f3b922ff4ebcb793b9dba05b79eb40723cc1
niveshnagar <niveshnagar23@gmail.com>
2024-09-05 01:10:24 +0530

    chore: added schema for Account

--- backend/db/index.js
+++ backend/db/index.js
@@ -38,8 +38,23 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+const AccountSchema = new mongoose.Schema({
+  // Referencing the user to prevent adding funds to non-existing users
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", UserSchema);
+const Account = mongoose.model("Account", AccountSchema);
 
 module.exports = {
   User,
+  Account,
 };
