38141b837a961c1dba37d13efd6b2f63abe635d3
niveshnagar <niveshnagar23@gmail.com>
2024-09-05 01:16:30 +0530

    chore: Initiated route mounting for "/account" route

--- backend/routes/v1/v1.router.js
+++ backend/routes/v1/v1.router.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+
 const userRouter = require("./user.router");
+const accountRouter = require("./account.router");
 
 const router = Router();
 
 // Add endpoints to router here...
 router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
 module.exports = router;
