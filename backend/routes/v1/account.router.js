48a571f3a9f45156d035f35429be56d2adeffeb2
niveshnagar <niveshnagar23@gmail.com>
2024-09-05 02:14:54 +0530

    chore: added an endpoint to get balance of signed in user

--- backend/routes/v1/account.router.js
+++ backend/routes/v1/account.router.js
@@ -1,5 +1,40 @@
 const { Router } = require("express");
 
+const { User, Account } = require("../../db");
+const userAuthMiddleware = require("../../middlewares/user/userAuth.middleware");
+
 const router = Router();
+
+// Route - Endpoint to get balance of a signed in user.
+router.get("/balance", userAuthMiddleware, async (req, res) => {
+  try {
+    const username = req.username;
+    const user = await User.findOne({ username });
+    const userId = user._id;
+
+    const accountInfo = await Account.findOne({ userId });
+    const balance = accountInfo.balance;
+
+    res.json({
+      message: "Request successful",
+      balance,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Oops something went wrong!",
+    });
+  }
+});
+
+// Route -
+router.post("/transfer", userAuthMiddleware, (req, res) => {
+  try {
+    
+  } catch (error) {
+    return res.status(500).json({
+      message: "Oops something went wrong!",
+    });
+  }
+});
 
 module.exports = router;
